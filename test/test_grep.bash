8b84d81a093c7e57ee0c8623ec6b27d746db76cf
Duong <s24c1069mq@s.chibakoudai.jp>
2026-01-08 11:28:46 +0900

    Fix

--- test/test_grep.bash
+++ test/test_grep.bash
@@ -2,30 +2,38 @@
 # SPDX-FileCopyrightText: 2025 Duong Huyen
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+source /opt/ros/humble/setup.bash
 
-cd $dir/ros2_ws
+if [ -d "/root/ros2_ws" ]; then
+    cd /root/ros2_ws
+elif [ -d "$HOME/ros2_ws" ]; then
+    cd $HOME/ros2_ws
+else
+    echo "Error: Could not find ros2_ws directory!"
+    exit 1
+fi
+
 colcon build
 source install/setup.bash
 
 ros2 run mypkg pattern_filter &
 FILTER_PID=$!
-sleep 1
+sleep 2  
 
 echo "hello ros" | ros2 run mypkg stream_publisher &
 PUB_PID=$!
-sleep 1
+sleep 2
 
-RESULT=$(timeout 2 ros2 topic echo /filtered_text --once)
+RESULT=$(timeout 5 ros2 topic echo /filtered_text --once)
 
 kill $FILTER_PID
 kill $PUB_PID
 
 if echo "$RESULT" | grep -q "hello ros"; then
-    echo "Test Passed: Filtered text received on topic"
+    echo "Test Passed: Correct output received"
     exit 0
 else
-    echo "Test Failed: No output on topic /filtered_text"
+    echo "Test Failed: Expected 'hello ros', but got nothing or wrong data"
+    echo "Debug info: Result was: $RESULT"
     exit 1
 fi
